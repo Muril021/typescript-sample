48cae11b523f976f5a1a12ed9392a7ecc9ac57a9
Murilo Nascimento <murilojesus659@gmail.com>
2022-09-15 16:10:57 -0300

    Exemplo TypeScript

---
+++ build/bundle.js
@@ -0,0 +1,11 @@
+var idade = 27;
+var nome = 'Murilo';
+var maiorDeIdade = true;
+function soma(num1, num2) {
+    return num1 + num2;
+    /* OUTRA FORMA DE USAR O RETURN:
+     * const soma:number=num1+num2;
+     * return soma;
+     */
+}
+document.write("A soma de 7+5 \u00E9 ".concat(soma(7, 5)));
---
+++ src/script.ts
@@ -0,0 +1,12 @@
+const idade:number=27;
+const nome:string='Murilo';
+const maiorDeIdade: boolean=true;
+
+function soma(num1:number, num2:number):number{
+   return num1+num2;
+   /* OUTRA FORMA DE USAR O RETURN:
+    * const soma:number=num1+num2;
+    * return soma;
+    */ 
+}
+document.write(`A soma de 7+5 é ${soma(7,5)}`);
